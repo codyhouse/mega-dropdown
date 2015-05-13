b7dad6432c73d63c8d75cdb13875bf2b470d505c
claudia-romano <claudia.romano06@gmail.com>
2015-05-13 16:15:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Mega Dropdown
+=========
+
+A responsive and easy to customise mega-dropdown component.
+
+[Article on CodyHouse](http://codyhouse.co/gem/mega-dropdown/)
+
+[Demo](http://codyhouse.co/demo/mega-dropdown/index.html)
+ 
+[Terms](http://codyhouse.co/terms/)
+
+Icons: [Nucleo Library](https://nucleoapp.com/)
---
+++ js/main.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function($){
+	//open/close mega-navigation
+	$('.cd-dropdown-trigger').on('click', function(event){
+		event.preventDefault();
+		toggleNav();
+	});
+
+	//close meganavigation
+	$('.cd-dropdown .cd-close').on('click', function(event){
+		event.preventDefault();
+		toggleNav();
+	});
+
+	//on mobile - open submenu
+	$('.has-children').children('a').on('click', function(event){
+		//prevent default clicking on direct children of .cd-dropdown-content 
+		if(!( $(this).parent('.has-children').parent('.cd-dropdown-content').length > 0 && window.getComputedStyle(document.querySelector('.cd-dropdown-wrapper'), '::before').getPropertyValue('content') == 'desktop' )) event.preventDefault();
+		var selected = $(this);
+		selected.next('ul').removeClass('is-hidden').end().parent('.has-children').parent('ul').addClass('move-out');
+	});
+
+	//on desktop - open submenu on hover
+	$('.cd-dropdown-content').children('.has-children').mouseenter(function(){
+		if( ! ($('.cd-dropdown-content .slide-in').length > 0) ) $('.cd-dropdown-content').children('.has-children').children('ul').addClass('slide-in');
+	}).mouseleave(function(){
+		if( $('li.has-children:hover').length == 0 || $('li.has-children:hover').is($(this))) $('.cd-dropdown-content .slide-in').removeClass('slide-in');
+	});
+
+	//submenu items - go back link
+	$('.go-back').on('click', function(){
+		var selected = $(this),
+			visibleNav = $(this).parent('ul').parent('.has-children').parent('ul');
+		selected.parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('move-out');
+	});
+
+	//IE9 placeholder fallback
+	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+		  	}
+		}).blur(function() {
+		 	var input = $(this);
+		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+		  	}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+		  	$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+			 		input.val('');
+				}
+		  	})
+		});
+	}
+
+	function toggleNav(){
+		var navIsVisible = ( !$('.cd-dropdown').hasClass('dropdown-is-active') ) ? true : false;
+		$('.cd-dropdown').toggleClass('dropdown-is-active', navIsVisible);
+		$('.cd-dropdown-trigger').toggleClass('dropdown-is-active', navIsVisible);
+		if( !navIsVisible ) {
+			$('.cd-dropdown').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('.has-children ul').addClass('is-hidden');
+				$('.move-out').removeClass('move-out');
+			});	
+		}
+	}
+});
